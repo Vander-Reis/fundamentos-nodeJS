dd772bcbad19c34cd5cf3a7cbff18a84d41d8869
Vander-Reis <vanderreis2017@outlook.com>
2022-12-15 20:45:12 -0300

    adicionando mais metodos

--- src/index.js
+++ src/index.js
@@ -128,5 +128,22 @@ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
 
     return response.json(customer);
-})
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    // splice 
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+});
 
